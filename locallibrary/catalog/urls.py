25f8255414bee60f65322ec161283407b56cef23
Andrew Woods <andrew.woods.1@ndus.edu>
2024-04-04 03:02:08 -0500

    Final website with tests

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,8 +5,35 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>',
+         views.AuthorDetailView.as_view(), name='author-detail'),
 ]
 
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),  # Added for challenge
+
+]
+
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
+
+# Add URLConf to list, view, create, update, and delete languages
+urlpatterns += [
+    path('languages/', views.LanguageListView.as_view(), name='languages'),
+    path('language/<int:pk>', views.LanguageDetailView.as_view(),
+         name='language-detail'),
+    path('language/create/', views.LanguageCreate.as_view(), name='language-create'),
+    path('language/<int:pk>/update/',
+         views.LanguageUpdate.as_view(), name='language-update'),
+    path('language/<int:pk>/delete/',
+         views.LanguageDelete.as_view(), name='language-delete'),
 ]
